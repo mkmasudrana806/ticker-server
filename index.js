a639caa6007f81dd3c47426431c82c250f710b14
mkmasudrana806 <mkmasudrana806@gmail.com>
2023-09-19 11:53:10 +0600

    fix: server to index.js

--- index.js
+++ index.js
@@ -17,9 +17,14 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://127.0.0.1:5500",
   })
 );
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: "http://127.0.0.1:5500",
+//   })
+// );
 
 app.get("/", (req, res) => {
   res.send("server is running");
